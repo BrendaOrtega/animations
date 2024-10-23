cf12810f4e552f583354201d4473652b9855c538
BrendaOrtega <bremin11.20.93@gmail.com>
2024-10-23 13:06:02 -0600

    tiphography ready

--- src/home/ScrollBanenr.tsx
+++ src/home/ScrollBanenr.tsx
@@ -4,14 +4,25 @@ import { Marquee } from "../components/Banners";
 export const ScrollBanner = () => {
   return (
     <>
-      <Marquee>
+      <Marquee className="jolly-lodger-regular bg-dark">
+        No más sitios web estáticos <span className="mx-3">🪄</span> Agrega
+        animaciones a tu sitio web
+        <span className="mx-3">🧙🏻</span> Personaliza tus animaciones
+        <span className="mx-3">🎃</span> Crea tus propios componentes
+        <span className="mx-3">🎛️</span>
         No más sitios web estáticos <span className="mx-3">🪄</span> Agrega
         animaciones a tu sitio web
         <span className="mx-3">🧙🏻</span> Personaliza tus animaciones
         <span className="mx-3">🤩</span> Crea tus propios componentes
         <span className="mx-3">🎛️</span>
       </Marquee>
-      <Marquee reversed className="bg-fish">
+      <Marquee reversed className="bg-fish jolly-lodger-regular">
+        Scroll Animations<span className="mx-3">🎐</span> Parallax
+        <span className="mx-3">💻</span> Efectos 3d{" "}
+        <span className="mx-3">🎯</span> Drawers<span className="mx-3">👀</span>{" "}
+        Galerías<span className="mx-3">🏞️</span> Dynamic cards{" "}
+        <span className="mx-3">🚀</span>Text effects{" "}
+        <span className="mx-3">💬</span> Modals <span className="mx-3">🔥</span>
         Scroll Animations<span className="mx-3">🎐</span> Parallax
         <span className="mx-3">💻</span> Efectos 3d{" "}
         <span className="mx-3">🎯</span> Drawers<span className="mx-3">👀</span>{" "}
